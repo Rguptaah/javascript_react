bfc2c8d624a4d712b6f60dd60ae4f9d47e8c8449
sidarya321@gmail.com <sidarya321@gmail.com>
2023-12-17 17:17:07 +0530

    variable in js

---
+++ variable.js
@@ -0,0 +1,15 @@
+const applicationId = 7468;
+let applicationEmail = "rahul@rahul.com";
+var applicationPassword = "12345";
+applicationCity = "Noida";
+let applicationState;
+
+// applicationId = 456; Its a contsant cannot  be changed
+console.log(applicationId);
+
+/*
+Prefer to not use of var
+because of issue in block scope and functional scope
+*/
+
+console.table([applicationId,applicationEmail,applicationPassword,applicationCity,applicationState]);

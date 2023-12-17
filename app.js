212e41510510f798aeaae8d1eb0d08e8d99db016
sidarya321@gmail.com <sidarya321@gmail.com>
2023-12-17 15:53:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+// console.log('hoo whats up! well working');
+
+// var start = 76;
+// var end = 100;
+
+// function starter(counter){
+//     if(counter >=start && counter < end){
+//         start++;
+//         console.log(`S:${start}`);
+//     }else if(counter == end){
+//         end--;
+//         if(end <= 76){
+//            start++; 
+//         }else{
+
+//         }
+//         // console.log(`S:${end}`);
+//     }
+// }
+
+// setInterval(function(){
+//   if(start < end){
+//    starter(start);
+//   }else{
+//     end--;
+//     starter(end);
+//     // console.log(`End:${end}`);
+//   }
+// },100)
+
+console.log(5*6);
+const obj = {
+    a:1,
+    b:"2",
+    c:[1,2,3,4,5,6]
+}
+console.log(typeof(obj.b));
+
+  

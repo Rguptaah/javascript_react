2508d698bf429daa8e45323e82184c1aefd0e7f0
sidarya321@gmail.com <sidarya321@gmail.com>
2023-12-17 17:46:16 +0530

    datatype

---
+++ basicsJS/dataType.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+4) // we are using nodeJS , not browser
+
+console.log(3
+    +3); // This is not right way of coding
+
+console.log('Rahul')
+
+
+// dataType
+
+// Number => 2 to the power 53
+// bigInt => For max value than number
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => undefined
+// Symbol => unique
+
+// object

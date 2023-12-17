cc78f6e36ac2e21ca54b970522d77aa6bc56a7b8
sidarya321@gmail.com <sidarya321@gmail.com>
2023-12-17 16:44:07 +0530

    modified some code

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
-// console.log('hoo whats up! well working');
-
-// var start = 76;
-// var end = 100;
-
-// function starter(counter){
-//     if(counter >=start && counter < end){
-//         start++;
-//         console.log(`S:${start}`);
-//     }else if(counter == end){
-//         end--;
-//         if(end <= 76){
-//            start++; 
-//         }else{
-
-//         }
-//         // console.log(`S:${end}`);
-//     }
-// }
-
-// setInterval(function(){
-//   if(start < end){
-//    starter(start);
-//   }else{
-//     end--;
-//     starter(end);
-//     // console.log(`End:${end}`);
-//   }
-// },100)
-
+console.log('hoo whats up! well working');
 console.log(5*6);
 const obj = {
     a:1,
